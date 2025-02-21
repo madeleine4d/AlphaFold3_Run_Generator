068868284f13ec96fd21b298c4c8281b97b5396f
Maddy <madeleiner4d@gmail.com>
2025-02-21 15:44:10 -0800

    initial script written

--- AlphaFold3Comparison.py
+++ AlphaFold3Comparison.py
@@ -1,4 +1,20 @@
 import pandas as pd
+import json
 
-with pd.read_csv('Seqs.csv') as seqs:
-    
+seqs = pd.read_csv('Seqs.csv')
+jobs = []
+print ('this is the data file you have imported:')
+print (seqs)
+for i in list(seqs.index.values):
+    job = {
+    "name": seqs.at[i, 'name'],
+    "modelSeeds": [],  
+    "sequences": [
+      {"growthFactor": {'sequence': seqs.at[i, 'gf'], 'count': 2}},
+      {"affibody": {'sequence': seqs.at[i, 'aff'], 'count': 1}},
+    ],
+    }
+    jobs.append(job)
+
+with open('jobs.json', 'w') as outfile:
+    json.dump(jobs, outfile)
